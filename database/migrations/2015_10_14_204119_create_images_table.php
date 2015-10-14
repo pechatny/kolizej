3cd61f6712b601db167406a9e5d1311f6263e252
Dmitry Pechatnikov <pechatny@gmail.com>
2015-10-15 00:03:48 +0300

    add structure elements

---
+++ database/migrations/2015_10_14_204119_create_images_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('product_id');
+            $table->string('image');
+            $table->string('image1');
+            $table->string('image2');
+            $table->string('image3');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('images');
+    }
+}
--- resources/views/admin/leftMenu.blade.php
+++ resources/views/admin/leftMenu.blade.php
@@ -1,5 +1,8 @@
 <div class="block">
     <a href="/admin/menu">Meню</a>
     <a href="/admin/pages">Страницы</a>
+    <a href="/admin/category">Категории товаров</a>
+    <a href="/admin/products">Товары</a>
+    <a href="/admin/colors">Цвета</a>
     <a href="/admin/menu">Контакты</a>
 </div>
