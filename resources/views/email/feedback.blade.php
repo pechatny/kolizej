21d27e8480166eb23d1e040b888bae182215f11c
Max <maksik123@mail.ru>
2015-11-08 18:40:46 +0300

    feedback template

--- resources/views/email/feedback.blade.php
+++ resources/views/email/feedback.blade.php
@@ -1,5 +1,12 @@
-<html><body style='font-family:Arial,sans-serif;'>
-<h2 style='font-weight:bold;border-bottom:1px dotted #ccc;'>Cообщение с сайта</h2>
-<p><strong>Имя:</strong>name</p>
-<p><strong>Телефон:</strong>phone</p>
-</body></html>
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+</head>
+<body>
+<table width="710" align="center" cellpadding="0" cellspacing="0" style="border-collapse:collapse; border:#cccccc 1px solid;">
+<tr><td border="0" style="border:none;">шапка</td></tr>
+<tr><td border="0" style="border:none;">тело письма</td></tr>
+<tr><td border="0" style="border:none; border-top:#1667b2 5px solid;">футер</td></tr>
+</table>
+</body>
+</html>
