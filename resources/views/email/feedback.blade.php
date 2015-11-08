f8281420efcb5cc25d6036ec4c045ecfb8407891
Max <maksik123@mail.ru>
2015-11-08 17:26:00 +0300

    check  feedback:

--- resources/views/email/feedback.blade.php
+++ resources/views/email/feedback.blade.php
@@ -1,26 +1,5 @@
-<html>
-    <head>
-        <title>Обратная связь [ kolizej.ru ]</title>
-    </head>
-    <body>
-        <h1>Обратная связь</h1>
-        <table>
-            <tr>
-                <td>Имя</td>
-                <td>{{$name}}</td>
-            </tr>
-            <tr>
-                <td>Телефон:</td>
-                <td>{{$phone}}</td>
-            </tr>
-            <tr>
-                <td>Email:</td>
-                <td>{{$mail}}</td>
-            </tr>
-            <tr>
-                <td>Сообщение:</td>
-                <td>{{$text}}</td>
-            </tr>
-        </table>
-    </body>
-</html>
+<html><body style='font-family:Arial,sans-serif;'>
+<h2 style='font-weight:bold;border-bottom:1px dotted #ccc;'>Cообщение с сайта</h2>
+<p><strong>Имя:</strong>name</p>
+<p><strong>Телефон:</strong>phone</p>
+</body></html>
