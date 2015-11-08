0d07b19ae109786344aa15d30c361dd258602f32
Max <maksik123@mail.ru>
2015-11-08 18:57:40 +0300

    feedback email template

--- resources/views/email/feedback.blade.php
+++ resources/views/email/feedback.blade.php
@@ -1,12 +1,33 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
 <html>
 <head>
-<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
 </head>
 <body>
-<table width="710" align="center" cellpadding="0" cellspacing="0" style="border-collapse:collapse; border:#cccccc 1px solid;">
-<tr><td border="0" style="border:none;">шапка</td></tr>
-<tr><td border="0" style="border:none;">тело письма</td></tr>
-<tr><td border="0" style="border:none; border-top:#1667b2 5px solid;">футер</td></tr>
-</table>
+    <h1></h1>
+    <table>
+        <tr>
+            <td>Имя</td>
+            <td>{{$name}}</td>
+        </tr>
+        <tr>
+            <td>Телефон:</td>
+            <td>{{$phone}}</td>
+        </tr>
+        <tr>
+            <td>Email:</td>
+            <td>{{$mail}}</td>
+        </tr>
+        <tr>
+            <td>Сообщение:</td>
+            <td>{{$text}}</td>
+        </tr>
+    </table>
+
+    <table width="710" align="center" cellpadding="0" cellspacing="0" style="border-collapse:collapse; border:#cccccc 1px solid;">
+    <tr><td border="0" style="border:none;">шапка</td></tr>
+    <tr><td border="0" style="border:none;">тело письма</td></tr>
+    <tr><td border="0" style="border:none; border-top:#1667b2 5px solid;">футер</td></tr>
+    </table>
 </body>
 </html>
