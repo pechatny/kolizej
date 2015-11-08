007a44f05f6b4cb715219d2eed65d13ae23d667c
Max <maksik123@mail.ru>
2015-11-08 19:03:12 +0300

    feedback email template

--- resources/views/email/feedback.blade.php
+++ resources/views/email/feedback.blade.php
@@ -4,30 +4,26 @@
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
 </head>
 <body>
-    <h1>Обращение из формы обратной связи</h1>
+    <h1 style="font-size:150%; margin-bottom:20px;">Обращение из формы обратной связи</h1>
     <table width="100%" align="center" cellpadding="2" cellspacing="0" border="1" style="border-collapse:collapse; border:#eeeeee 1px solid;">
         <tr>
-            <td>Имя</td>
-            <td>{{$name}}</td>
+            <td style="width:150px; border-collapse:collapse; border:#eeeeee 1px solid;">Имя</td>
+            <td style="border-collapse:collapse; border:#eeeeee 1px solid;">{{ $name }}</td>
         </tr>
         <tr>
-            <td>Телефон:</td>
-            <td>{{$phone}}</td>
+            <td style="width:150px; border-collapse:collapse; border:#eeeeee 1px solid;">Телефон:</td>
+            <td style="border-collapse:collapse; border:#eeeeee 1px solid;">{{ $phone }}</td>
         </tr>
         <tr>
-            <td>Email:</td>
-            <td>{{$mail}}</td>
+            <td style="width:150px; border-collapse:collapse; border:#eeeeee 1px solid;">Email:</td>
+            <td style="border-collapse:collapse; border:#eeeeee 1px solid;">
+                <a href="mailto:{{ $mail }}">{{ $mail }}</a>
+            </td>
         </tr>
         <tr>
-            <td>Сообщение:</td>
-            <td>{{$text}}</td>
+            <td style="width:150px; border-collapse:collapse; border:#eeeeee 1px solid;">Сообщение:</td>
+            <td style="border-collapse:collapse; border:#eeeeee 1px solid;">{{ $text }}</td>
         </tr>
-    </table>
-
-    <table width="710" align="center" cellpadding="0" cellspacing="0" style="border-collapse:collapse; border:#cccccc 1px solid;">
-    <tr><td border="0" style="border:none;">шапка</td></tr>
-    <tr><td border="0" style="border:none;">тело письма</td></tr>
-    <tr><td border="0" style="border:none; border-top:#1667b2 5px solid;">футер</td></tr>
     </table>
 </body>
 </html>
