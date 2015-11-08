4c56d45e71d42f119bd5331590e5ca8fa52bd10e
Max <maksik123@mail.ru>
2015-11-08 16:50:20 +0300

    feedback template

--- resources/views/email/feedback.blade.php
+++ resources/views/email/feedback.blade.php
@@ -1,20 +1,26 @@
-<p>Обратная связь:</p>
-
-<table>
-    <tr>
-        <td>Имя</td>
-        <td>{{$name}}</td>
-    </tr>
-    <tr>
-        <td>Телефон:</td>
-        <td>{{$phone}}</td>
-    </tr>
-    <tr>
-        <td>Email:</td>
-        <td>{{$mail}}</td>
-    </tr>
-    <tr>
-        <td>Сообщение:</td>
-        <td>{{$text}}</td>
-    </tr>
-</table>
+<html>
+    <head>
+        <title>Обратная связь [ kolizej.ru ]</title>
+    </head>
+    <body>
+        <h1>Обратная связь</h1>
+        <table>
+            <tr>
+                <td>Имя</td>
+                <td>{{$name}}</td>
+            </tr>
+            <tr>
+                <td>Телефон:</td>
+                <td>{{$phone}}</td>
+            </tr>
+            <tr>
+                <td>Email:</td>
+                <td>{{$mail}}</td>
+            </tr>
+            <tr>
+                <td>Сообщение:</td>
+                <td>{{$text}}</td>
+            </tr>
+        </table>
+    </body>
+</html>
